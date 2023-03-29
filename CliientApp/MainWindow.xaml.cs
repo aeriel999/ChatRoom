5187d2f6e80c9fe85cad7dab567bf5644638a897
aeriel999 <ok4743@gmail.com>
2023-03-29 11:12:47 +0300

    Add project files.

---
+++ CliientApp/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CliientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IPEndPoint serverEndPoint;
+        UdpClient client;
+
+        ObservableCollection<MessegeInfo> messeges = new ObservableCollection<MessegeInfo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = messeges;
+
+            client = new UdpClient();
+
+            string serverAdress = ConfigurationManager.AppSettings["ServerAddress"]!;
+            short serverPort = short.Parse(ConfigurationManager.AppSettings["ServerPort"])!;
+
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverAdress), serverPort);
+        }
+
+        private async void SendBtnClick(object sender, RoutedEventArgs e)
+        {
+            string msg = msgTB.Text;
+            SendMsg(msg);
+        }
+
+        private void JoinBtnClick(object sender, RoutedEventArgs e)
+        {
+            string msg = "$<join>";
+            SendMsg(msg);
+            Listen();
+        }
+
+        private async void SendMsg(string msg)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+
+            await client.SendAsync(data, data.Length, serverEndPoint);
+        }
+
+        private async void Listen()
+        { 
+           while (true) 
+            {
+                IPEndPoint? remoteIp = null;
+                var result = await client.ReceiveAsync();
+
+                string msg = Encoding.UTF8.GetString(result.Buffer);
+
+                messeges.Add(new MessegeInfo(msg));
+            }
+        }
+    }
+
+   public class MessegeInfo
+    {
+        public string Messege { get; set; }
+        public DateTime Time { get; set; }
+
+        public MessegeInfo(string text)
+        {
+            Messege = text;
+            Time = DateTime.Now;
+        }
+
+        public override string ToString() 
+        {
+            return $"{Messege} : {Time.ToShortTimeString()}";
+        }
+    }
+}
---
+++ Server_App/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string serverAdress = "127.0.0.1";
+const short serverPort = 4040;
+const string JOIN_CMD = "$<join>";
+
+
+
+HashSet<IPEndPoint> members = new HashSet<IPEndPoint>();
+
+UdpClient server = new UdpClient(serverPort);
+
+IPEndPoint clientEndPoint = null;
+
+while (true)
+{
+    byte[] data = server.Receive(ref clientEndPoint);
+
+    string msg = Encoding.UTF8.GetString(data);
+
+    Console.WriteLine($"Got : {msg} at {DateTime.Now.ToShortTimeString()} from {clientEndPoint}");
+
+    switch (msg)
+    {
+        case JOIN_CMD:
+            AddMember(clientEndPoint);
+            break;
+        default:
+            SendToAll(data);
+            break;
+    }
+}
+
+void AddMember(IPEndPoint member)
+{
+    members.Add(member);
+
+}
+
+void SendToAll(byte[] data)
+{
+    foreach (var m in members)
+    {
+        server.SendAsync(data, data.Length, m);
+    }
+}
